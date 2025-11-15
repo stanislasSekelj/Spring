f3dc44713e5f04558057af34c357f75bcf7b34db
Stan <stanislas.sekelj@gmail.com>
2025-11-15 18:51:00 +0000

    git initializr

---
+++ springclass/springclass/src/test/java/com/springboot/demo/springclass/SpringclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.demo.springclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringclassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
